4284cf8aae2338c8c09ca86937deb3560cdbd814
Nidhi Patel <111096326+nidhi-09@users.noreply.github.com>
2025-04-19 01:28:19 -0400

    Update highlight-invalid.directive.ts
    Added comments

--- src/app/highlight-invalid.directive.ts
+++ src/app/highlight-invalid.directive.ts
@@ -1,3 +1,4 @@
+/*ElementRef is a wrapper around the  DOM element it lets you directly access and manipulate html element*/
 import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
 import { NgControl } from '@angular/forms';
 
@@ -8,7 +9,7 @@ export class HighlightInvalidDirective {
 
   constructor(private el: ElementRef, private renderer: Renderer2, private control: NgControl) { }
 
-  @HostListener('input')
+  @HostListener('input')      //Hostlistener is a decorator that you use to listen to events on the element its applied to
   onInput() {
     if (this.control && this.control.invalid && this.control.touched) {
       this.renderer.setStyle(this.el.nativeElement, 'border', '2px solid red');
